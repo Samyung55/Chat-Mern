98995c609cfd38c060b76220366bec218feb0988
Sam Fred <samyung05@gmail.com>
2023-05-23 18:22:04 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const { signup, login } = require('../controls/auth');
 
-con
+const router = express.Router()
+
+router.post('/signup', signup);
+router.post('/login', login);
+
+module.exports = router;
