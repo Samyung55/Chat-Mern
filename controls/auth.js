8cf02975d26024d9a541c492e97019bc47367874
Sam Fred <samyung05@gmail.com>
2023-05-23 18:11:19 -0400

    Update auth.js

--- controls/auth.js
+++ controls/auth.js
@@ -33,6 +33,15 @@ const login = async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        const
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ name: username })
+        
+        if(!users.length) return res.status(400).json({ message: 'User not Found'})
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        
     }
 }
