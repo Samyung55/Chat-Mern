3d3813752f299d8a7280127f2159c98be1740bed
Sam Fred <samyung05@gmail.com>
2023-05-27 18:36:58 -0400

    Update Auth.jsx

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -30,6 +30,68 @@ const Auth = () => {
 
         const URL = 'https://localhost:5000/auth';
 
-        
+        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName: form.fullName, phoneNumber, avatarURL,
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
     }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+    }
+
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-fields-content">
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={handleSubmit}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-container__input">
+                                <label htmlFor='fullName'>Full Name</label>
+                                <input name='fullName'
+                                type='text'
+                                placeholder='Full Name'
+                                onChange={handleChange}
+                                required
+                                />
+                            </div>
+                        )
+                        }
+                        <div className="auth__form-container_fields-container_input">
+                            <label htmlFor='username'>Username</label>
+                            <input type="text" name='username' placeholder={handleChange}
+                            required />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor='phoneNumber'>Phone Number</label>
+                                <input name='phoneNumber'
+                                type='text' placeholder='Phone Number'
+                                onChange={handleChange} required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                
+                            </div>
+                        )}
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
