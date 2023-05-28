7f14a5e11ef5f998dbb0a674648cd5ee227ecc3f
Sam Fred <samyung05@gmail.com>
2023-05-28 15:07:48 -0400

    Create ChannelInner.jsx

---
+++ client/src/components/ChannelInner.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { MessageList, MessageInput, Thread, Window, useChannelActionContext, Avatar, useChannelStateContext, useChatContext } from 'stream-chat-react';
+
+import { ChannelInfo } from '../assets/ChannelInfo';
+
