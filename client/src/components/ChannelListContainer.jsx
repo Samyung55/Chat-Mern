0406cff6dc432cc94c7ef5be1c0a880069f91586
Sam Fred <samyung05@gmail.com>
2023-05-30 06:02:06 -0400

    Update ChannelListContainer.jsx

---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
+
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
+    
+)
