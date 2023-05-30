9c604213e6a33db5efd222548bf48e5d29a8c489
Sam Fred <samyung05@gmail.com>
2023-05-30 06:53:39 -0400

    Update ChannelSearch.jsx

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -11,4 +11,26 @@ const ChannelSearch = ({ setToggleContainer }) => {
     const [teamChannels, setTeamChannels] = useState([])
     const [directChannels, setDirectChannels] = useState([])
 
+    useEffect(() => {
+        if(!query) {
+            setTeamChannels([]);
+            setDirectChannels([]);
+        }
+    }, [query])
+
+    const getChannels = async (text) => {
+        try {
+            const channelResponse = client.queryChannels({
+                type: 'team',
+                name: { $autocomplete: text },
+                members: { $in: [client.userID]}
+            });
+            const userResponse = client.queryUsers({
+                id: { $ne: client.userID },
+                name: { $autocomplete: text }
+            })
+
+            const [channels, { users }] = await Promise.all([channelResponse, userResponse]);
+        }
+    }
 }
