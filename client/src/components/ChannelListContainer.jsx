7ca5d2633367b278ef68fa6605985d6b31417f7c
Sam Fred <samyung05@gmail.com>
2023-05-30 06:09:47 -0400

    Update ChannelListContainer.jsx
    Create Company Header

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -22,3 +22,10 @@ const SideBar = ({ logout }) => (
         </div>
     </div>
 );
+
+const CompanyHeader = () => (
+    <div className="channel-list__header">
+        <p className="channel-list__header__text">Medical Pager</p>
+    </div>
+)
+
