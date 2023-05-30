435b1cb714a112a420ae282d455fbac1ba50bcdb
Sam Fred <samyung05@gmail.com>
2023-05-30 06:46:48 -0400

    Update ChannelListContainer.jsx

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -121,7 +121,11 @@ const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) =>
         <div className="channel-list__container-responsive"
         style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff" }}
         >
-            
+            <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
+                
+            </div>
+            <ChannelListContent setIsCreating={setIsCreating} setCreateType={setCreateType} setIsEditing={setIsEditing}
+            setToggleContainer={setToggleContainer} />
         </div>
         </>
     )
