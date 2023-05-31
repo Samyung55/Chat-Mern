ac36bfd324fb242863c23a8c68826b693e2e9c4d
Sam Fred <samyung05@gmail.com>
2023-05-31 08:14:54 -0400

    Create TeamChannelPreview.jsx

---
+++ client/src/components/TeamChannelPreview.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Avatar, useChatContext } from 'stream-chat-react';
+
+const TeamChannelPreview = ({ setActiveChannel, setIsCreating, setIsEditing, setToggleContainer, channel, type }) => {
+    
+}
