7f9d916f1dc556cd74fb81ee997cf2b874275bca
Sam Fred <samyung05@gmail.com>
2023-05-31 07:17:02 -0400

    Create CreateChannel.jsx

---
+++ client/src/components/CreateChannel.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import { UserList } from './Index';
+import { CloseCreateChannel } from '../assets';
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+
+}
