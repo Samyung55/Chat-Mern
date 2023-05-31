36b96697359d7e46fb235e31d7ad7b71ef7899dd
Sam Fred <samyung05@gmail.com>
2023-05-31 08:16:49 -0400

    Update TeamChannelPreview.jsx

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import { Avatar, useChatContext } from 'stream-chat-react';
 
 const TeamChannelPreview = ({ setActiveChannel, setIsCreating, setIsEditing, setToggleContainer, channel, type }) => {
+    const { channel: setActiveChannel, client } = useChatContext();
+
+    const ChannelPreview = () => (
+        <p className="channel-preview__item">
+            # {channel?.data?.name || channel?.data?.id}
+        </p>
+    );
+
     
 }
