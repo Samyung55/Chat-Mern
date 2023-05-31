02f86e06dfe4f90895c47cc550fa44a3420c3100
Sam Fred <samyung05@gmail.com>
2023-05-31 08:20:58 -0400

    Update TeamChannelPreview.jsx

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -10,5 +10,19 @@ const TeamChannelPreview = ({ setActiveChannel, setIsCreating, setIsEditing, set
         </p>
     );
 
-    
+    const DirectPreview = () => {
+        const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+
+        console.log(members[0]);
+
+        return (
+            <div className="channel-preview__item single">
+                <Avatar image={members[0]?.user?.image}
+                name={members[0]?.user?.fullName || members[0]?.user?.id}
+                size={24}
+                />
+                <p>{members[0]?.user?.fullName || members[0]?.user?.id}</p>
+            </div>
+        )
+    }
 }
