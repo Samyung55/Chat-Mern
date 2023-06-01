23f717e70bdac579257cedc58095bc60383ce0a7
Sam Fred <samyung05@gmail.com>
2023-06-01 16:13:44 -0400

    Update UserList.jsx

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -4,5 +4,14 @@ import { Avatar, useChatContext } from 'stream-chat-react';
 import { InviteIcon } from '../assets';
 
 const ListContainer = ({ children }) => {
-    
+    return (
+        <div className="user-list__contaiine">
+            <div className="user-list__header">
+                <p>User</p>
+                <p>Invite</p>
+            </div>
+            {children}
+        </div>
+    )
 }
+
