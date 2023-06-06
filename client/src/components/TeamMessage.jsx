344b46afbf0096064af1bf45364ad4a08a42429a
Sam Fred <samyung05@gmail.com>
2023-06-06 17:21:03 -0400

    Round Up

--- client/src/components/TeamMessage.jsx
+++ client/src/components/TeamMessage.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { MessageTeam, useMessageContext } from 'stream-chat-react';
+import { useMessageContext } from 'stream-chat-react';
 
 const TeamMessage = () => {
-    const { handleOpenThread, message } = useMessageContext();
+  const { message } = useMessageContext();
 
-    return (
-        <MessageTeam
-            message={{ ...message, user: {}}}
-            // handleOpenThread={}
-        />
-    )
-}
+  return <div key={message.id}>{message.text}</div>;
+};
 
-export default TeamMessage
+export default TeamMessage;
